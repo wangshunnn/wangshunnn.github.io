2cc405e9ccfc2168c91ad98b7d897bc434bc809a
wangshunnn <wangshunnn@gmail.com>
2025-04-13 15:37:42 +0800

    chore: update og image

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -28,14 +28,14 @@ export default defineConfig({
     ['meta', { property: 'og:description', content: description }],
     [
       'meta',
-      { property: 'og:image', content: 'https://soonwang.me/og/index.svg' }
+      { property: 'og:image', content: 'https://soonwang.me/og/index.png' }
     ],
     ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
     ['meta', { name: 'twitter:title', content: title }],
     ['meta', { name: 'twitter:description', content: description }],
     [
       'meta',
-      { name: 'twitter:image', content: 'https://soonwang.me/og/index.svg' }
+      { name: 'twitter:image', content: 'https://soonwang.me/og/index.png' }
     ],
     ['meta', { name: 'twitter:site', content: '@wangshunnn' }]
   ],
